7d82f5c333aff72ec9aaef503013ccb1bb3af1b0
Kalashgirigoswami <168511033+Kalashgirigoswami@users.noreply.github.com>
2024-06-01 10:40:50 +0530

    Add files via upload

---
+++ Kk.java
@@ -0,0 +1,14 @@
+
+class Kk
+{
+    public static void main(String args[])
+    {
+
+        Main m1=new Main();
+        m1.setRollno(15);
+        m1.setName("kalash");
+        System.out.println("roll no.:"+m1.getRollno());
+        System.out.println("name:"+m1.getName());
+
+    }
+}
